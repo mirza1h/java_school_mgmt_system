d9272a7a2fbc2e059297599cf79ad7f95ab8af1a
Mirza Hasanovic <mirza.hasanovic1@fet.ba>
2019-06-18 23:53:43 +0200

    DB test

--- src/views/Main.java
+++ src/views/Main.java
@@ -13,10 +13,20 @@ public class Main {
 	private static final String PERSISTENCE_UNIT_NAME = "RazvojSoftvera";
 	private static EntityManagerFactory factory;
 
+	private static void setDBSystemDir() {
+		// Decide on the db system directory: <userhome>/.addressbook/
+		String userHomeDir = System.getProperty("user.home", ".");
+		String systemDir = userHomeDir + "/.addressbook";
+		System.out.println(systemDir);
+		// Set the db system directory.
+		System.setProperty("derby.system.home", systemDir);
+	}
+
 	public static void main(String[] args) {
 		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 		EntityManager em = factory.createEntityManager();
 		// read the existing entries and write to console
+		setDBSystemDir();
 		Query q = em.createQuery("select t from Todo t");
 		List<Todo> todoList = q.getResultList();
 		for (Todo todo : todoList) {
