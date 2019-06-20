b8d5948fedec6635c1e804f2e46acc5eb6907d1c
Sead Delic <sead.delic1@fet.ba>
2019-06-20 17:33:49 +0200

    login gui

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import java.awt.Color;
 import java.awt.Polygon;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -11,12 +12,15 @@ import java.util.List;
 import com.sun.javafx.scene.control.skin.Utils;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
@@ -89,48 +93,81 @@ public class Main extends Application {
 		return novi;
 	}
 
+	public void startLoginPage(Stage primaryStage) throws IOException {
+		VBox login = FXMLLoader.load(getClass().getResource("login.fxml"));
+		Scene loginScene = new Scene(login);
+
+		loginScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setResizable(false);
+
+		primaryStage.setScene(loginScene);
+		primaryStage.show();
+
+		AnchorPane mainPane = (AnchorPane) login.getChildren().get(0);
+		AnchorPane duals = (AnchorPane) mainPane.getChildren().get(1);
+		AnchorPane loginPane = (AnchorPane) duals.getChildren().get(1);
+
+		Button bezPrijave = (Button) loginPane.getChildren().get(1);
+
+		bezPrijave.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		        try {
+					startRasporedPage(primaryStage, false);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+		    }
+		});
+	}
+
+	public void startRasporedPage(Stage primaryStage, boolean registrovan) throws IOException {
+
+		VBox root = FXMLLoader.load(getClass().getResource("raspored.fxml"));
+		Scene scene = new Scene(root);
+
+		ScrollPane scrollPane = (ScrollPane) root.getChildren().get(0);
+		AnchorPane mainPane = (AnchorPane) scrollPane.getContent();
+		AnchorPane weekDaysPane = (AnchorPane) mainPane.getChildren().get(1);
+		AnchorPane drawPane = (AnchorPane) mainPane.getChildren().get(3);
+		AnchorPane defaultBlock = (AnchorPane) drawPane.getChildren().get(12);
+
+		AnchorPane novi = newBlock(defaultBlock);
+
+		defaultBlock.setVisible(true);
+
+		/*
+		 * factory =
+		 * Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		 * EntityManager em = factory.createEntityManager();
+		 * setDBSystemDir();
+		 */
+
+		drawPane.getChildren().add(novi);
+
+		List<Node> allDays = weekDaysPane.getChildren();
+
+		List<String> weekDates = getDates();
+		for (int i = 0; i < 6; ++i) {
+			AnchorPane day = (AnchorPane) allDays.get(i);
+			Label dateLabel = (Label) day.getChildren().get(1);
+			dateLabel.setText(weekDates.get(i));
+
+		}
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setResizable(false);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
 	@Override
 	public void start(Stage primaryStage) {
 
 		try {
-			// BorderPane root = new BorderPane();
-			VBox root = FXMLLoader.load(getClass().getResource("raspored.fxml"));
-			Scene scene = new Scene(root);
 
-			ScrollPane scrollPane = (ScrollPane) root.getChildren().get(0);
-			AnchorPane mainPane = (AnchorPane) scrollPane.getContent();
-			AnchorPane weekDaysPane = (AnchorPane) mainPane.getChildren().get(1);
-			AnchorPane drawPane = (AnchorPane) mainPane.getChildren().get(3);
-			AnchorPane defaultBlock = (AnchorPane) drawPane.getChildren().get(12);
+			startLoginPage(primaryStage);
 
-			AnchorPane novi = newBlock(defaultBlock);
-
-			defaultBlock.setVisible(true);
-
-			/*
-			 * factory =
-			 * Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-			 * EntityManager em = factory.createEntityManager();
-			 * setDBSystemDir();
-			 */
-
-			drawPane.getChildren().add(novi);
-
-			List<Node> allDays = weekDaysPane.getChildren();
-
-			List<String> weekDates = getDates();
-			for (int i = 0; i < 6; ++i) {
-				AnchorPane day = (AnchorPane) allDays.get(i);
-				Label dateLabel = (Label) day.getChildren().get(1);
-				dateLabel.setText(weekDates.get(i));
-
-			}
-
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setResizable(false);
-
-			primaryStage.setScene(scene);
-			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
